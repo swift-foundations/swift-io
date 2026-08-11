675a635a54f8972abda2dbc479bc4b5ba3309680
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 07:23:09 +0200

    Compose typed duplex IO channels

---
+++ Sources/IO/IO.Reader.swift
@@ -0,0 +1,6 @@
+public import Async_Channel_Primitives
+
+extension IO {
+    /// The single-consumer inbound half of an ``IO/Channel``.
+    public typealias Reader<Element: ~Copyable, Failure: Swift.Error & Sendable> = Async.Channel<Element, Failure>.Receiver
+}
---
+++ Sources/IO/IO.Writer.swift
@@ -0,0 +1,6 @@
+public import Async_Channel_Primitives
+
+extension IO {
+    /// The copyable outbound half of an ``IO/Channel``.
+    public typealias Writer<Element: ~Copyable, Failure: Swift.Error & Sendable> = Async.Channel<Element, Failure>.Sender
+}
