aa1883d3ddeb945b19cd4a6669290a5072280909
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 09:32:58 +0200

    Adopt Swift 6.4 package baseline

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -22,11 +22,11 @@ let eventsTests: [Target] = {
 let package = Package(
     name: "swift-io",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27)
     ],
     products: [
         .library(name: "IO", targets: ["IO"]),
